c2302c6eee3d60674f958e15670b8b9720205a66
thjbbvlt <thibault2@ik.me>
2024-06-06 09:36:56 +0200

    class Annotater

--- litteralement/insert.py
+++ litteralement/insert.py
@@ -1,53 +1,77 @@
 import silex
 import psycopg
-from utils import get_labels_updated, get_feats
-import pipeline
+from litteralement.utils import get_labels_updated, get_feats
+import litteralement.pipeline
 
 
-DBNAME = "litteralement"
-conn = psycopg.connect(dbname=DBNAME)
+class Annotater:
+    def __init__(self, dbname="litteralement", **kwargs):
+        conn = psycopg.connect(dbname=dbname)
 
-nlp = pipeline.nlp
+        nlp = litteralement.pipeline.load_model(**kwargs)
 
-# récupérer les labels du modèle.
-dep_labels = nlp.get_pipe("parser").labels
-pos_labels = silex.lexique.POS_NAMES_LOWER
-feats_labels = get_feats(nlp)
+        dep_labels = nlp.get_pipe("parser").labels
+        pos_labels = silex.lexique.POS_NAMES_LOWER
+        feats_labels = get_feats(nlp)
 
-# récupérer les valeurs existantes dans la base de données:
-# - pos (nature)
-pos = get_labels_updated(conn, "nature", pos_labels.values())
-# - dep (fonction)
-dep = get_labels_updated(conn, "fonction", dep_labels)
-# - morph (morphologie au format FEATS)
-morph = get_labels_updated(
-    conn, "morph", feats_labels, colname="feats"
-)
-# - lemmes
-lemmas = get_labels_updated(conn, "lemme", {}, colname="graphie")
+        pos = get_labels_updated(conn, "nature", pos_labels.values())
+        dep = get_labels_updated(conn, "fonction", dep_labels)
+        morph = get_labels_updated(
+            conn, "morph", feats_labels, colname="feats"
+        )
+        lemmas = get_labels_updated(
+            conn, "lemme", {}, colname="graphie"
+        )
 
-# construire les objets de lookup qui incrémentent automatiquement les ids.
-lookup_pos = silex.LookupTable(pos)
-lookup_dep = silex.LookupTable(dep)
-lookup_morph = silex.LookupTable(morph)
-lookup_lemma = silex.LookupTable(lemmas)
+        self.lookup_pos = silex.LookupTable(pos)
+        self.lookup_dep = silex.LookupTable(dep)
+        self.lookup_morph = silex.LookupTable(morph)
+        self.lookup_lemma = silex.LookupTable(lemmas)
 
-# token_underscore = ["tokentype"]
-# lexeme_underscore = ["vv_morph", "vv_pos"]
+        if (
+            "include_tokentype" in kwargs
+            and kwargs["include_tokentype"] is not False
+        ):
 
-lexique = silex.Lexique(
-    get_pos=lambda token: lookup_pos[token.pos_],
-    get_lemma=lambda token: lookup_lemma[token.lemma_],
-    get_morph=lambda token: lookup_morph[str(token.morph)],
-    get_norm=lambda token: token.norm_,
-    add_token_attrs=lambda token: {
-        "dep": lookup_dep[token.dep_],
-        "tokentype": token._.tokentype,
-    },
-    add_lex_attrs=lambda token: {
-        "vv_morph": token._.vv_morph,
-        "vv_pos": token._.vv_pos,
-    },
-)
+            def add_token_attrs(token):
+                return {
+                    "dep": self.lookup_dep[token.dep_],
+                    "tokentype": token._.tokentype,
+                }
+        else:
 
-conn.close()
+            def add_token_attrs(token):
+                return {"dep": self.lookup_dep[token.dep_]}
+
+        if (
+            "include_viceverser" in kwargs
+            and kwargs["include_viceverser"] is not False
+        ):
+
+            def add_lex_attrs(token):
+                return {
+                    "vv_morph": token._.vv_morph,
+                    "vv_pos": token._.vv_pos,
+                }
+        else:
+
+            def add_lex_attrs(token):
+                return {}
+
+        self.lexique = silex.Lexique(
+            get_pos=lambda token: self.lookup_pos[token.pos_],
+            get_lemma=lambda token: self.lookup_lemma[token.lemma_],
+            get_morph=lambda token: self.lookup_morph[
+                str(token.morph)
+            ],
+            get_norm=lambda token: token.norm_,
+            add_token_attrs=add_token_attrs,
+            add_lex_attrs=add_lex_attrs,
+        )
+
+    def __call__(self, doc, **kwargs):
+        return self.lexique(doc, **kwargs)
+
+    def pipe(self, docs, **kwargs):
+        for i in docs:
+            yield self(i, **kwargs)
--- litteralement/pipeline.py
+++ litteralement/pipeline.py
@@ -1,16 +1,25 @@
 from spacy.language import Language
 import spacy
+import silex
+from litteralement.utils import get_labels_updated, get_feats
 
 
 @Language.component("sent_on_newline")
 def newline_is_new_sentence(doc):
+    """Ajoute des sentence boundaries sur les nouvelles lignes.
+
+    Args:
+        doc (Doc)
+    Returns (Doc)
+    """
+
     for token in doc:
         if token.text == "\n":
             token.is_sent_start = True
     return doc
 
 
-def load(
+def load_model(
     model="fr_core_news_lg",
     include_presque={},
     include_tokentype={},
@@ -19,7 +28,13 @@ def load(
     include_sent_on_newline=True,
     **kwargs,
 ):
+    """Charge un modèle.
+
+    Returns (Language):  le modèle pour analyser avec spacy.
+    """
+
     nlp = spacy.load(model, **kwargs)
+
     if include_presque is not False:
         import presque
 
@@ -50,10 +65,16 @@ def load(
             after="morphologizer",
             config=include_viceverser_lemmatizer,
         )
+
     return nlp
 
 
-def load_default():
+def load_model_default():
+    """Charge le modèle avec les paramètres par défaut.
+
+    Returns (Language):  un modèle pour l'analyse avec spacy.
+    """
+
     model = "fr_core_news_lg"
     exclude_disable = ["ner", "lemmatizer"]
     presque_config = dict(
@@ -88,7 +109,7 @@ def load_default():
         "dr",
     ]
 
-    nlp = load(
+    nlp = load_model(
         model=model,
         exclude=exclude_disable,
         disable=exclude_disable,
@@ -98,4 +119,46 @@ def load_default():
         include_sent_on_newline=True,
         include_viceverser_lemmatizer=True,
     )
+
     return nlp
+
+
+def load_silex(conn, nlp, **kwargs):
+    # récupérer les labels du modèle.
+    dep_labels = nlp.get_pipe("parser").labels
+    pos_labels = silex.lexique.POS_NAMES_LOWER
+    feats_labels = get_feats(nlp)
+
+    # récupérer les valeurs existantes dans la base de données:
+    # - pos (nature)
+    pos = get_labels_updated(conn, "nature", pos_labels.values())
+    # - dep (fonction)
+    dep = get_labels_updated(conn, "fonction", dep_labels)
+    # - morph (morphologie au format FEATS)
+    morph = get_labels_updated(
+        conn, "morph", feats_labels, colname="feats"
+    )
+    # - lemmes
+    lemmas = get_labels_updated(conn, "lemme", {}, colname="graphie")
+
+    # construire les objets de lookup qui incrémentent automatiquement les ids.
+    lookup_pos = silex.LookupTable(pos)
+    lookup_dep = silex.LookupTable(dep)
+    lookup_morph = silex.LookupTable(morph)
+    lookup_lemma = silex.LookupTable(lemmas)
+
+    lexique = silex.Lexique(
+        get_pos=lambda token: lookup_pos[token.pos_],
+        get_lemma=lambda token: lookup_lemma[token.lemma_],
+        get_morph=lambda token: lookup_morph[str(token.morph)],
+        get_norm=lambda token: token.norm_,
+        add_token_attrs=lambda token: {
+            "dep": lookup_dep[token.dep_],
+            "tokentype": token._.tokentype,
+        },
+        add_lex_attrs=lambda token: {
+            "vv_morph": token._.vv_morph,
+            "vv_pos": token._.vv_pos,
+        },
+    )
+    return lexique
