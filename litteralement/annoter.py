490e8ccb8c5b27ae4539c6ced584ccdf2d3f3ad5
thjbbvlt <thibault2@ik.me>
2024-06-09 04:49:19 +0200

    fonction insert (copy, copy, copy)

--- litteralement/annoter.py
+++ litteralement/annoter.py
@@ -1,8 +1,17 @@
-import tqdm
 import json
+import tqdm
 from psycopg.sql import SQL
+import psycopg
 from litteralement.lang.fr import todict
 from litteralement.statements import copy_to_multicolumns
+from litteralement.lookups import get_lemma_lookup
+from litteralement.lookups import get_pos_lookup
+from litteralement.lookups import get_dep_lookup
+from litteralement.lookups import get_morph_lookup
+from litteralement.lookups import get_lexeme_lookup
+from litteralement.annoter import copy_to_mot
+from litteralement.annoter import copy_to_token
+from litteralement.annoter import copy_to_phrase
 
 
 def annoter(
@@ -76,16 +85,19 @@ def copy_to_from_temp(conn, table, key, columns):
 
     cur_get = conn.cursor()
     cur_send = conn.cursor()
-    sql_get = SQL("select _id, j from _temp_doc")
-    sql_copy_send = copy_to_multicolumns(table=table, columns=columns)
+    sql_get = SQL("select id, j from _temp_doc")
+    # okeee fonctionne pas..
+    sql_copy_send = copy_to_multicolumns(
+        table=table, columns=["texte"] + columns
+    )
     docs = cur_get.execute(sql_get)
     with cur_send.copy(sql_copy_send) as copy:
         for i in docs:
             textid = i[0]
             doc = i[1]
             for x in doc[key]:
-                row = (x[c] for c in columns)
-                copy.write_row((textid) + row)
+                row = tuple((x[c] for c in columns))
+                copy.write_row((textid,) + row)
 
 
 def copy_to_mot(conn):
@@ -110,6 +122,93 @@ def copy_to_phrase(conn):
     """Ajoute les phrases dans la base de données."""
 
     table = "phrase"
-    key = "phrase"
+    key = "phrases"
     columns = ["debut", "fin"]
     copy_to_from_temp(conn=conn, table=table, key=key, columns=columns)
+
+
+def insert():
+    conn = psycopg.connect(dbname="litteralement")
+
+    lookup_lemma = get_lemma_lookup(conn)
+    lookup_pos = get_pos_lookup(conn)
+    lookup_dep = get_dep_lookup(conn)
+    lookup_morph = get_morph_lookup(conn)
+    lookup_lex = get_lexeme_lookup(conn)
+
+    conn.execute("create temp table _temp_doc (id int, j jsonb);")
+    cur_get = conn.cursor()
+    cur_send = conn.cursor()
+
+    def numerize_lex(lex):
+        """Numerise les lexèmes.
+
+        Args:
+            lex (dict):  le dict décrivant le lexème.
+
+        Returns (int):  l'id du lexème.
+
+        Durant le processus, les POS (nature), DEP (fonction) et LEMMA (lemme) sont aussi analysés. ils sont ajoutés dans leurs tables lookups respectives, à partir desquelles ils seront ajoutés dans la base de données.
+        """
+
+        for (
+            tag,
+            lookup,
+        ) in (
+            ("nature", lookup_pos),
+            ("lemme", lookup_lemma),
+            ("morph", lookup_morph),
+        ):
+            val = lex[tag]
+            lex[tag] = lookup[val]
+        key = lookup_lex.Key(**lex)
+        _id = lookup_lex[key]
+        return _id
+
+    def numerize_mot(word):
+        """Numérise un mot."""
+
+        dep = lookup_dep[word["fonction"]]
+        lex = numerize_lex(word["lexeme"])
+        head = word["noyau"]
+        word.update(
+            {
+                "lexeme": lex,
+                "fonction": dep,
+                "noyau": head,
+            }
+        )
+        return word
+
+    def numerize_doc():
+        docs = cur_get.execute(
+            "select id, j from import._document limit 1000;"
+        )
+        with cur_send.copy("copy _temp_doc (id, j) from stdin") as copy:
+            for i in docs:
+                _id = i[0]
+                j = i[1]
+                words = j["mots"]
+                for word in words:
+                    _ = numerize_mot(word)
+                copy.write_row(
+                    (
+                        _id,
+                        json.dumps(j),
+                    )
+                )
+
+    numerize_doc()
+
+    lookup_morph.copy_to()
+    lookup_pos.copy_to()
+    lookup_dep.copy_to()
+    lookup_lemma.copy_to()
+    lookup_lex.copy_to()
+
+    copy_to_mot(conn)
+    copy_to_token(conn)
+    copy_to_phrase(conn)
+
+    conn.commit()
+    conn.close()
