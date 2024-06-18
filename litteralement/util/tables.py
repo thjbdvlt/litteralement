55e4bbf0ba75f17acaed8142a15484a0b0a1b750
thjbbvlt <thibault2@ik.me>
2024-06-18 17:10:31 +0200

    fichier avec VARIABLES

---
+++ litteralement/util/tables.py
@@ -0,0 +1,32 @@
+DOC_TABLE = "import._document"
+LEXEME_TEXT_TABLE = "_lexeme_text"
+LEXEME_ATTRS = [
+    {
+        "name": "id",
+        "is_literal": True,
+        "datatype": "integer",
+    },
+    {
+        "name": "lemme",
+        "value_column": "graphie",
+        "datatype": "text",
+        "is_literal": False,
+    },
+    {
+        "name": "morph",
+        "value_column": "feats",
+        "datatype": "text",
+        "is_literal": False,
+    },
+    {
+        "name": "nature",
+        "value_column": "nom",
+        "datatype": "text",
+        "is_literal": False,
+    },
+    {
+        "name": "norme",
+        "datatype": "text",
+        "is_literal": True,
+    },
+]
