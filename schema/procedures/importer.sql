e22dfd51e395842bf252405c32d03864b580522b
thjbbvlt <thibault2@ik.me>
2024-06-21 09:45:41 +0200

    corr. import.sql EAV

--- schema/procedures/importer.sql
+++ schema/procedures/importer.sql
@@ -37,7 +37,8 @@ as $$
         -- déplie les relations
         create temp table _relation_text as
         select
-            import_id as sujet,
+            -- import_id as sujet,
+            id as sujet,
             r.*
         from _ent e,
         jsonb_to_recordset(e.relations) as r(type text, objet text);
@@ -51,11 +52,12 @@ as $$
         -- ajoute les relations
         insert into relation (sujet, type, objet)
         select
-            l1.id as sujet,
+            -- l1.id as sujet,
+            r.sujet,
             y.id as type,
             l2.id as objet
         from _relation_text r
-        join import._lookup_ent l1 on l1.import_id = r.sujet
+        -- join import._lookup_ent l1 on l1.import_id = r.sujet
         join import._lookup_ent l2 on l2.import_id = r.objet
         join type_relation y on y.nom = r.type;
 
