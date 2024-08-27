cbe936fbc7f478efc3be1514227106b818b36164
thjbbvlt <thibault2@ik.me>
2024-08-27 09:43:01 +0200

    formatting

--- scripts/installer-tokentype.sh
+++ scripts/installer-tokentype.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # installer le module 'tokentype'
+
+set -e
+
 repo=https://github.com/thjbdvlt/tokentype
 temp=/tmp/litteralement-installer
+
 git clone $repo $temp
+
 cd $temp
+
 pip install .
