3ef8057d8a5497221207ee80d52e75e668413b2a
thjbbvlt <thibault2@ik.me>
2024-09-02 20:07:42 +0200

    wrote command line options

--- litteralement/__main__.py
+++ litteralement/__main__.py
@@ -1,27 +1,60 @@
 import argparse
-from . import sql
 
 
 if __name__ == "__main__":
+    # command line argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "command",
-        choices=("generate-schema", "join-schema", "annotate", "copy"),
+        choices=("schema", "annotate", "copy", "join-schema"),
     )
-    parser.add_argument(
-        "-s",
-        "--schema",
-        type=str,
-        choices=("litteralement", "eav"),
-        nargs=2,
+
+    # copy
+    group_copy = parser.add_argument_group("copy")
+    group_copy.add_argument(
+        "file",
+        nargs="+",
+        help="files to copy data from (JSON/JSONL).",
     )
-    parser.add_argument(
+    group_copy.add_argument(
+        "-l",
+        "--jsonl",
+        actions="store_true",
+        default=False,
+        required=False,
+        help="use jsonl file format (one objet per line).",
+    )
+
+    # print schema
+    group_schema = parser.add_argument_group("print-schema")
+    group_schema.add_argument(
+        "schema",
+        choices=(
+            "eav",
+            "litteralement",
+            "both",
+        ),
+        nargs=1,
+        action="store",
+        help='schema to be output: "eav", "litteralement" or both.',
+    )
+    group_schema.add_argument(
         "-t",
         "--text",
+        help='the table containing texts, referenced by tables "mot", "phrase", "span", "segment" and "token". this is used to create referencing constraint.',
         type=str,
-        nargs="?",
+        action="store",
+        metavar="SCHEMA.TABLE.ID_COLUMN",
     )
-    schema = sql.get_schema_definition()
+
+    # join-schema
+    group_join = parser.add_argument_group("join-schema")
+    group_join.add_argument(
+        "table",
+        nargs=1,
+        help='the table containing texts, referenced by tables "mot", "phrase", "span", "segment" and "token". this is used to create referencing constraint.',
+    )
+
 
 # litteralement schema generate
 # litteralement schema join
