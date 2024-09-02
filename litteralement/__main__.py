6073f90ed0d5c54fd1f51bb5686e9d1222e0a35a
thjbbvlt <thibault2@ik.me>
2024-09-02 20:29:20 +0200

    add subparsers

--- litteralement/__main__.py
+++ litteralement/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+from .util import tables
 
 
 if __name__ == "__main__":
@@ -9,42 +10,55 @@ if __name__ == "__main__":
         choices=("schema", "annotate", "copy", "join-schema"),
     )
 
-    # copy
-    group_copy = parser.add_argument_group("copy")
-    group_copy.add_argument(
+    # subparser for subcommands
+    subparsers = parser.add_subparsers()
+    sub_copy = subparsers.add_parser(
+        "copy",
+        help=f"copy data from files into the {tables.SCHEMA_EAV} schema tables.",
+    )
+    sub_schema = subparsers.add_parser(
+        "schema",
+        help="print the definition of a schema to STDOUT.",
+    )
+    sub_annotate = subparsers.add_parser(
+        "annotate",
+        help=f"annotate texts using spacy and insert the resulting annotations into the tables of the schema {tables.SCHEMA}",
+    )
+    sub_join = subparsers.add_parser(
+        "join",
+        help='generate the SQL commands to create referencing constraints (for the tables "mot", "token", "phrase", "span" and "segment"). it is only usefull if a database already has the schema "litteralement", but that schema is not referencing the table containing texts.',
+    )
+
+    sub_copy.add_argument(
         "file",
         nargs="+",
         help="files to copy data from (JSON/JSONL).",
     )
-    group_copy.add_argument(
+    sub_copy.add_argument(
         "-l",
         "--jsonl",
-        actions="store_true",
+        action="store_true",
         default=False,
         required=False,
         help="use jsonl file format (one objet per line).",
     )
 
     # print schema
-    group_schema = parser.add_argument_group("print-schema")
-    group_schema.add_argument(
-        "schema",
-        choices=(
-            "eav",
-            "litteralement",
-            "both",
-        ),
-        nargs=1,
+    sub_schema.add_argument(
+        "name",
+        choices=(tables.SCHEMA_EAV, tables.SCHEMA, "both"),
+        nargs="?",
         action="store",
-        help='schema to be output: "eav", "litteralement" or both.',
+        help="name of the schema to be output.",
+        default="both",
     )
-    group_schema.add_argument(
+    sub_schema.add_argument(
         "-t",
-        "--text",
+        "--text-table",
         help='the table containing texts, referenced by tables "mot", "phrase", "span", "segment" and "token". this is used to create referencing constraint.',
         type=str,
         action="store",
-        metavar="SCHEMA.TABLE.ID_COLUMN",
+        metavar="SCHEMA.TABLE.PK",
     )
 
     # join-schema
@@ -54,6 +68,12 @@ if __name__ == "__main__":
         nargs=1,
         help='the table containing texts, referenced by tables "mot", "phrase", "span", "segment" and "token". this is used to create referencing constraint.',
     )
+
+    # annotate
+    group_annotate = parser.add_argument_group("annotate")
+
+    args = parser.parse_args()
+    # print(args)
 
 
 # litteralement schema generate
